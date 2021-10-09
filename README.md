0594866325381bc2011559bd027f4d58bf540ff3
Mahamudur Rahaman Khan <khan.mahmud@outlook.com>
2021-10-09 21:55:42 +0600

    call/sms alert

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Embedded system development with various sensors and microcontrollers
     1. MQ-4 Gas Sensor
     2. 2x US-015 Ultrasonic Module
     3. MG996 Servo Motor
+    4. A9G GPS & GPRS Module
--- smartDustbin/smartDustbin.ino
+++ smartDustbin/smartDustbin.ino
@@ -1,116 +1,162 @@
 #include <Servo.h>
-const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
-const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+
+const int echoPin = 5; // Echo Pin of Ultrasonic Sensor
+const int pingPin = 6; // Trigger Pin of Ultrasonic Sensor
 const int echoPin2 = 9; // Echo Pin of Ultrasonic Sensor
-const int pingPin2 = 8; // Trigger Pin of Ultrasonic Sensor
-const int AO_Pin=0; // Connect AO of MQ4 with Analog channel 0 pin (A0) of Arduino
-int AO_Out; // stores analog output of MQ4 sensor
+const int pingPin2 = 10; // Trigger Pin of Ultrasonic Sensor
+const int gasPin=0; // MQ4 with Analog channel 0 pin (A0) of Arduino
+const int servoPin = 3; // PWM pin for SERVO motor
+const int greenLED = 2; // Normal indicator
+const int orangeLED = 4; // Lid action indicator
+const int redLED = 7; // Gas Hazard indicator
 
 
-Servo motor;
-int pointer = 0;
-void setup() {
-  motor.attach(2, 2000, 2400);
-  ServoClose();
-  Serial.begin(9600);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT); 
-}
-void ServoClose()
+
+Servo lidServo; // Servo motor
+int pos = 0; // initial Servo rotation
+int dialLimit = 1; // Send SMS only once
+int sentCount = 0; // Counter variable to send SMS only once
+char phoneNumber[] = "01784190980";  //phone number to alert
+int gasSaturation = 0; // Analogue output of the MQ4 sensor
+int trashDistance = 50;
+int remainingCapacity = 50;
+
+void setup() 
 {
-  for (pointer = 420; pointer >= 0; pointer -= 10) { 
-    motor.write(pointer);
-  }
-}
-
-void ServoOpen()
-{
-  for (pointer = 0; pointer <= 420; pointer += 10) {
-    motor.write(pointer);  
-  }
-}
-
-long microsecondsToInches(long microseconds) {
-   return microseconds / 74 / 2;
-}
-
-
-void lidAction(){
-   long duration, inches;
-   pinMode(pingPin, OUTPUT);
-   digitalWrite(pingPin, LOW);
-   delayMicroseconds(2);
-   digitalWrite(pingPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(pingPin, LOW);
-   pinMode(echoPin, INPUT);
-   duration = pulseIn(echoPin, HIGH);
-   inches = microsecondsToInches(duration);
-   Serial.print("Distance: ");
-   Serial.print(inches);
-   Serial.print(" in");
-   Serial.println();
-   if(inches < 8){
-    ServoOpen();
-  digitalWrite(11, HIGH);   
-  delay(2500); 
-   }
-   else{
-  digitalWrite(11, LOW);   
-  ServoClose();
-    }   
-  }
-
-
-void capacityAlert(){
-   long duration, inches;
-   pinMode(pingPin2, OUTPUT);
-   digitalWrite(pingPin2, LOW);
-   delayMicroseconds(2);
-   digitalWrite(pingPin2, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(pingPin2, LOW);
-   pinMode(echoPin2, INPUT);
-   duration = pulseIn(echoPin2, HIGH);
-   inches = microsecondsToInches(duration);
-   Serial.print("Remaining Distance: ");
-   Serial.print(inches);
-   Serial.print(" in");
-   Serial.println();
-   if(inches < 3){
-  ServoOpen();
-  digitalWrite(11, HIGH);
-  digitalWrite(12, LOW);
-  delay(4000);     
-   }
-   else{
-  digitalWrite(11, LOW); 
-  digitalWrite(12, HIGH); 
-  ServoClose();
-    }
-      
-  }  
-
-void gasAlert(){
-  AO_Out= analogRead(AO_Pin);
-  Serial.print("Methane Conentration: ");
-  Serial.println(AO_Out);
-  if (AO_Out >  300){
-  digitalWrite(13, HIGH);
-  digitalWrite(12, LOW);
+  lidServo.attach(servoPin);
+  closeLid();
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("AT+CMGF=1"); //set GSM to text mode
   delay(200);
- }
-  else{
-  digitalWrite(13, LOW);
-  digitalWrite(12, HIGH);
-} 
+  pinMode(greenLED, OUTPUT);
+  pinMode(orangeLED, OUTPUT);
+  pinMode(redLED, OUTPUT); 
 }
 
-void loop() {
-  digitalWrite(12, HIGH);
+void openLid()
+{
+  for(pos = 0; pos<= 170; pos+=8)
+  {
+    lidServo.write(pos);
+    delay(30); 
+  }
+}
+
+void closeLid()
+{
+  for(pos; pos >= 0; pos-=8)
+  {
+    lidServo.write(pos);
+    delay(20);
+  }
+
+}
+
+long microsecondsToInches(long microseconds) 
+{
+  return microseconds / 74 / 2;
+}
+
+
+void collectTrash()
+{
+  long duration;
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  trashDistance = microsecondsToInches(duration);
+  
+  if(trashDistance < 7)
+  {
+    digitalWrite(orangeLED, HIGH);
+    openLid();
+    delay(3000); 
+  }
+  else
+  {
+    digitalWrite(orangeLED, LOW);   
+    closeLid();
+  }   
+}
+
+
+void capacityAlert()
+{
+  long duration;
+  pinMode(pingPin2, OUTPUT);
+  digitalWrite(pingPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin2, LOW);
+  pinMode(echoPin2, INPUT);
+  duration = pulseIn(echoPin2, HIGH);
+  remainingCapacity = microsecondsToInches(duration);
+  
+  if(remainingCapacity <= 3)
+  {
+    openLid();
+    digitalWrite(orangeLED, HIGH);
+    digitalWrite(greenLED, LOW);
+    delay(3000);     
+  }
+  else
+  {
+    digitalWrite(greenLED, HIGH);    
+    digitalWrite(orangeLED, LOW);  
+    closeLid();
+  }
+}  
+
+
+void gasAlert()
+{
+  gasSaturation = analogRead(gasPin);
+  if (gasSaturation > 250)
+  {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(redLED, HIGH);
+    while(sentCount<dialLimit)
+    {
+      delay(1500);
+      Serial.print("ATD");; //AT Dial phone number
+      Serial.print(phoneNumber);
+      Serial.println(";"); // Append Semicolon to execute command
+      delay(15000);
+      
+      Serial.println("ATH"); // Hang up in a few seconds
+      delay(1000);
+      
+      Serial.print("AT+CMGS=\"");
+      Serial.print(phoneNumber);
+      Serial.println("\"");
+
+      delay(1000);  // Wait for '>'
+
+      Serial.print("DANGER!!! Hazardous GAS detected near your SmartDustbin!");  // Alert SMS
+      Serial.write(0x1A);  // HEX code for ctrl+z send the SMS
+      delay(5000);
+      
+      sentCount++;
+    }
+  }
+  else
+  {
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(redLED, LOW);
+  }  
+}
+
+void loop() 
+{ 
+  digitalWrite(greenLED, HIGH);
+  collectTrash();
   capacityAlert();
-  lidAction();
   gasAlert();
-   
 }
